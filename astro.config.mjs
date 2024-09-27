673e0861487d01ee51e98d8cee11f56cc204a662
ali hussain <ali@gizmo.ai>
2024-09-27 16:56:49 +0100

    Adding light & dark themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-tutorial-ali.netlify.app/"
+  site: "https://astro-tutorial-ali.netlify.app/",
+  integrations: [preact()]
 });
